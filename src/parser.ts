d6b13c22536945501bf8423757dd82226e79a3d9
Christiaan Scheermeijer <christiaan@videodock.com>
2024-04-19 23:28:56 +0200

    fix: set custom cwd for vinyl-fs

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,4 @@
-import { join } from 'node:path';
+import { dirname, join, relative } from 'node:path';
 import { execSync } from 'node:child_process';
 import { Transform } from 'node:stream';
 import vfs from 'vinyl-fs';
@@ -16,12 +16,26 @@ export async function parseTranslations(projectPath: string, config: UserConfig,
     .filter((file) => !file.includes('node_modules') && file !== '' && !file.endsWith('.d.ts'));
 
   if (verbose) console.info(colors.cyan(`  [tsc] found ${sources.length} source files`));
+  // should the cwd be passed via cli args?
+  // This is not the process cwd, but used for scanning files with absolute paths outside the actual cwd
+  //  option 1: determine the cwd (current)
+  //  option 2: add option for cli option `--cwd ../../` (might be confusing)
+  //  option 3: run script via `"i18next": "cd ../../ && i18next-workspaces ./packages/project"`
+  const sourceCwd = sources.map(dirname).reduce((previousValue, currentValue) => {
+    const rel = relative(previousValue, currentValue);
+    return rel.startsWith('..') ? join(previousValue, rel.match(/(\.\.\/?)+/)?.[0] || '') : previousValue;
+  }, cwd);
+
+  if (relative(cwd, sourceCwd).length > 0) {
+    console.info(colors.yellow('IMPORTANT: ') + colors.dim(`Using a different cwd for vinyl-fs: ` + sourceCwd));
+    console.info('');
+  }
 
   let count = 0;
 
   return new Promise<void>((resolve, reject) => {
     vfs
-      .src(sources)
+      .src(sources, { cwd: sourceCwd })
       .pipe(
         (new I18nextTransform(config) as Transform)
           .on('reading', function (file: StreamFile) {
